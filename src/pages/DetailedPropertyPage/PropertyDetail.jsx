0863143fd1c42cb480f261a1bcd032426b9c883b
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-08-08 14:31:01 +0530

    Update PropertyDetail.jsx

--- src/pages/DetailedPropertyPage/PropertyDetail.jsx
+++ src/pages/DetailedPropertyPage/PropertyDetail.jsx
@@ -16,7 +16,7 @@ const PropertyDetail = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/properties/${id}`);
+        const response = await axios.get(`https://server-hvns.onrender.com/api/properties/${id}`);
         setProperty(response.data);
       } catch (error) {
         setError(error.message);
